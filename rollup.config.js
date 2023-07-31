1af9b791bdc0fc4142ff879eb691895060613c54
Phyllalintang <phyllalintang@gmail.com>
2023-07-31 13:18:11 +0700

    chore: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+import path from "path";
+
+const { LERNA_PACKAGE_NAME } = process.env;
+const PACKAGE_ROOT_PATH = process.cwd();
+const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, "package.json"));
+const INPUT_FILE = path.join(PACKAGE_ROOT_PATH, `${PKG_JSON.srcFile}/${PKG_JSON.srcName}.ts`);
+const OUTPUT_DIR = path.join(PACKAGE_ROOT_PATH, "dist");
+const IS_BROWSER_BUNDLE = !!PKG_JSON.browser;
+const formats = IS_BROWSER_BUNDLE ? ["umd"] : ["es", "cjs"];
+
+export default formats.map(format => ({
+  plugins: [
+    nodeResolve(),
+    commonjs({
+      include: /node_modules/,
+    }),
+    babel({
+      exclude: ["node_modules/**"],
+      presets: [
+        [
+          '@babel/preset-env', 
+          {'modules': false}
+        ], 
+        '@babel/react'
+      ],
+      plugins: ["styled-jsx/babel"],
+      babelHelpers: 'bundled'
+    }),
+    typescript({ 
+      compilerOptions: {
+        lib: ["es5", "es6", "dom"], 
+        target: "es5"
+      },
+      module: "ESNext"
+    })
+  ],
+  input: INPUT_FILE,
+  external: [
+    'react',
+    'react-dom',
+  ],
+  output: {
+    ...(format === "umd" ? {file: path.join(OUTPUT_DIR, `${PKG_JSON.srcName}.${format}.ts`)} : {}),
+    format,
+    sourcemap: true,
+    name: LERNA_PACKAGE_NAME,
+    amd: {
+      id: LERNA_PACKAGE_NAME
+    },
+    ...(format !== "umd" ? {dir: OUTPUT_DIR} : {}),
+  },
+  ...(format !== "umd" ? {preserveModules: true} : {}),
+}));
+  
